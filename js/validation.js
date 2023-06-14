23b98b5f7569cb4449059ac29865e04fb0325cb3
Alex Stelmakh <alex@mba.local>
2023-06-14 11:30:10 +0100

    Fix validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,10 +1,10 @@
 function validateForm() {
-    var emailInput = document.getElementById('email');
-    var emailError = document.getElementById('emailError');
-    var email = emailInput.value;
+    let emailInput = document.getElementById('email');
+    let emailError = document.getElementById('emailError');
+    let email = emailInput.value;
     // SQL keywords and Regular Expression to prevent malicious requests
-    var sqlKeywords = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'TABLE'];
-    var validEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let sqlKeywords = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'TABLE'];
+    let validEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     // Validate email format
     if (!validEmailRegex.test(email)) {
@@ -13,10 +13,10 @@ function validateForm() {
     }
 
     // Check for SQL injection keywords in other fields
-    var formFields = document.getElementById('contactForm').elements;
-    for (var i = 0; i < formFields.length; i++) {
-        var fieldValue = formFields[i].value;
-        for (var j = 0; j < sqlKeywords.length; j++) {
+    let formFields = document.getElementById('contactForm').elements;
+    for (let i = 0; i < formFields.length; i++) {
+        let fieldValue = formFields[i].value;
+        for (let j = 0; j < sqlKeywords.length; j++) {
             if (fieldValue.toUpperCase().includes(sqlKeywords[j])) {
                 formFields[i].value = '';
                 formFields[i].style.borderColor = 'red';
